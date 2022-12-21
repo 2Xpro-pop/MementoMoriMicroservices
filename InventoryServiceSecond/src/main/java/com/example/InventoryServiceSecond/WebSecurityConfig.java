4c218c201ea3c63e12a6e8968e5fd8eef48c5b0e
2Xpro-pop <bay.asanov@mail.ru>
2022-12-21 08:29:35 +0600

    bug fixing

---
+++ InventoryServiceSecond/src/main/java/com/example/InventoryServiceSecond/WebSecurityConfig.java
@@ -0,0 +1,2 @@
+package com.example.InventoryServiceSecond;public class WebSecurityConfig {
+}
---
+++ Patients/src/main/java/com/example/Patients/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.InventoryServiceSecond;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .build();
+    }
+}
